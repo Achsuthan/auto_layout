19b9d13006450641a23675860f8a8c7f187f8cef
Achsuthan <achsuthan@icloud.com>
2020-01-01 11:02:27 +0530

    1. Image container added 2. image added inside the image container 3. TextView top anchor added related with the ImageTopContainer
    >>> ach

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -36,6 +36,8 @@ class ViewController: UIViewController {
         textView.isSelectable = false
         //Disable scrollable
         textView.isScrollEnabled = false
+        //Set the text to textview
+        textView.text = "Join us today in our fun and games!"
         return textView
     }()
     
@@ -44,39 +46,58 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         //Add the imageview to view
-        view.addSubview(bearImageView)
+        //Removed becase of need to add this image view to image container
+        //view.addSubview(bearImageView)
         view.addSubview(lblTextView)
         
         setupLayout()
     }
     
     private func setupLayout(){
-        
+        //Create a container for put the iamge into all the screen sizes
         let topImageContainerView = UIView()
+        //Add the image container to view
         view.addSubview(topImageContainerView)
+        //Enable auto layout
         topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        //Set the top anchor from top view
         topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        //Set the leading anchor from view leading anchor
         topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        //Set the trailling acchor from view trilling anchor
         topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        //Set the height of the view with the 0.5 multyplier of device height anchor
         topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
+        //Add the image view to topImagecontainer
+        topImageContainerView.addSubview(bearImageView)
+        //Set the image to center align with image container
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        //Set the iamge height related with topimage container with the multiplier of 0.5
+        bearImageView.heightAnchor.constraint(lessThanOrEqualTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
         
-        //This is for center y
-        //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        //Set the top value
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        //This is for center x
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        //Set the width for image
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        //Set the height for image
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        //.isActive = true is used to activate the constrain
+        /**
+         //Commented becase it should be related with the top image container
+         //This is for center y
+         //imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+         //Set the top value
+         bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+         //This is for center x
+         bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+         //Set the width for image
+         bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+         //Set the height for image
+         bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+         //.isActive = true is used to activate the constrain
+         ***/
         
-        //Set the text to textview
-        lblTextView.text = "Join us today in our fun and games!"
-        //Set the top anchor from ImageView bottom 100
-        lblTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 100).isActive = true
+        
+        
+        //Set the top anchor from TopImageContiner bottom 0
+        lblTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor, constant: 0).isActive = true
+        //Commented its shoubld be based on the topImagecontainer view
+        //lblTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 100).isActive = true
         //Left side will start from left side of the view
         lblTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         //Right side end up with the right side of the view
